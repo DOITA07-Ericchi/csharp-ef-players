c4222fa94c56cebad40419c2d4e298c31c1bd6e7
EricchiYukia <git@ericchi.moe>
2022-12-22 17:13:07 +0100

    Modifiche al team

---
+++ Entity Framework Players/Team.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Entity_Framework_Players {
+	// Milestone 0
+	[Table ("player")]
+	[Index (nameof (id), IsUnique = true)]
+	public class Player {
+		[Key]
+		public int id { get; set; }
+		[Required]
+		public string nome { get; set; }
+		public string cognome { get; set; }
+		public int punteggio { get; set; }
+		public int nPartiteGiocate { get; set; }
+		public int nPartiteVinte { get; set; }
+	}
+}
+
